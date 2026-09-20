b17d2ad21a728db9722a8aa2206aac5df8bcf9ab
dzn4lk <23531@wc.school.nz>
2026-09-20 12:06:59 +1200

    CREATED DATABASE CALLED PLAYERS, CREATED NEW TABLE FOR TO SAVE PLAYER ID AND PLAYER NAME. CREATED APP.PY TO CONNECT TO MY DATABASE AND RETURN PLAYER. CREATED HTML PAGE TO SHOW LIST OF PLAYERS.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db():
+    conn = sqlite3.connect("players.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/players")
+def players():
+    conn = get_db()
+    players = conn.execute("SELECT * FROM players").fetchall()
+    conn.close()
 
+    return render_template("players.html", players=players)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
